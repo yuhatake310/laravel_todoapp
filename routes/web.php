e3331f9b2641f2692e1708118ab8c22717cd124d
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-08-12 16:20:55 +0900

    [add]ホームページを作成

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+Route::get('/', 'HomeController@index')->name('home');
+
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
