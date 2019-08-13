256e31363379204c3b7d274520006377e584d682
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-08-14 02:57:04 +0900

    [update]パスワード再設定のバリデーションメッセージを日本語化、メール内容変更

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -144,6 +144,10 @@ return [
     |
     */
 
-    'attributes' => [],
+    'attributes' => [
+        'email' => 'メールアドレス',
+        'password' => 'パスワード',
+        'token' => 'トークン',
+    ],
 
 ];
